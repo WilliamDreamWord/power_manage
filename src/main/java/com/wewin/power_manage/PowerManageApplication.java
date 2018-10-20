91e96f99c10808266b72f3ea03533628188ef9b7
william <2601865991@qq.com>
2018-10-20 10:05:03 +0800

    Initial commit

---
+++ src/main/java/com/wewin/power_manage/PowerManageApplication.java
@@ -0,0 +1,12 @@
+package com.wewin.power_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerManageApplication.class, args);
+    }
+}
